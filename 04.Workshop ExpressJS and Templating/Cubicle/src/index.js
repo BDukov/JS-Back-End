3119289da3304d149250f9aed35ff5ffdfa83728
Borislav Dukov <borislav.dukov@icloud.com>
2023-05-22 19:45:39 +0300

    add handlebars to project

--- 04.Workshop ExpressJS and Templating/Cubicle/src/index.js
+++ 04.Workshop ExpressJS and Templating/Cubicle/src/index.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const hendlebars = require('express-handlebars');
 
 const app = express();
 const PORT = 5000;
 
+//handlebars setup
+app.engine('hbs', hendlebars.engine({
+    extname: 'hbs',
+  //  layoutsDir: 'src/views/layouts',
+}));
+app.set('view engine', 'hbs');
+app.set('views', 'src/views');
+//routes
+
 app.get('/', (req, res) => {
-    res.send('Hello from express');
+    res.render('index');
 });
 
 app.listen(PORT, () =>{
