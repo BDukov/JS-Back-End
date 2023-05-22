64dc0910ce7b962277a934cb5fb8a87f58ba544b
Borislav Dukov <borislav.dukov@icloud.com>
2023-05-22 23:05:36 +0300

    add search

--- 04.Workshop ExpressJS and Templating/Cubicle/src/controllers/homeController.js
+++ 04.Workshop ExpressJS and Templating/Cubicle/src/controllers/homeController.js
@@ -5,7 +5,8 @@ const cubeManager = require('../managers/cubeManager');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    const cubes = cubeManager.getAll();
+    const {search, from, to} = req.query;
+    const cubes = cubeManager.getAll(search, from, to);
 
     res.render('index', { cubes });
 });
--- 04.Workshop ExpressJS and Templating/Cubicle/src/managers/cubeManager.js
+++ 04.Workshop ExpressJS and Templating/Cubicle/src/managers/cubeManager.js
@@ -15,7 +15,22 @@ const cubes = [
         difficultyLevel: 4
     }
 ];
-exports.getAll = () => cubes.slice();
+exports.getAll = (search, from, to) => {
+    let result = cubes.slice();
+
+    if (search) {
+        result = result.filter(cube => cube.name.toLocaleLowerCase().includes(search.toLocaleLowerCase()));
+    }
+
+    if (from) {
+        result = result.filter(cube => cube.difficultyLevel >= Number(from));
+    }
+
+    if (to) {
+        result = result.filter(cube => cube.difficultyLevel <= Number(to));
+    }
+    return result;
+}
 
 exports.getOne = (cubeId) => cubes.find(x => x.id == cubeId);
 
