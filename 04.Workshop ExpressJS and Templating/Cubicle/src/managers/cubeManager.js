09e185fce5066a31d52e21dc12215ccb3e2b5505
Borislav Dukov <borislav.dukov@icloud.com>
2023-05-29 20:37:29 +0300

    Get cubes from DB

--- 04.Workshop ExpressJS and Templating/Cubicle/src/managers/cubeManager.js
+++ 04.Workshop ExpressJS and Templating/Cubicle/src/managers/cubeManager.js
@@ -1,23 +1,10 @@
+const Cube = require('../models/Cube');
 const uniqid = require('uniqid');
-const cubes = [
-    {
-        id: 'u609xaxwlhz8k9dj',
-        name: 'The Punisher',
-        desctiprion: 'The Punisher is a fictional character appearing in American comic books published by Marvel Comics. The character was created by writer Gerry Conway and artists John Romita Sr. and Ross Andru, with publisher Stan Lee green-lighting the name.',
-        imageUrl: 'https://upload.wikimedia.org/wikipedia/en/5/5f/Punisher_%28Frank_Castle%29.jpg',
-        difficultyLevel: 6
-    },
-    {
-        id: 'u609xaxwld8sdgsj',
-        name: 'The Mirror',
-        description: 'The Mirror is a fictional character appearing in American comic books published by Marvel Comics. The character was created by writer Gerry Conway and artists John Romita Sr. and Ross Andru, with publisher Stan Lee green-lighting the name.',
-        imageUrl: 'https://upload.wikimedia.org/wikipedia/en/5/5f/Punisher_%28Frank_Castle%29.jpg',
-        difficultyLevel: 4
-    }
-];
-exports.getAll = (search, from, to) => {
-    let result = cubes.slice();
 
+exports.getAll = async (search, from, to) => {
+    let result = await Cube.find().lean();
+
+    //TODO: use mongooose to filter in DB
     if (search) {
         result = result.filter(cube => cube.name.toLocaleLowerCase().includes(search.toLocaleLowerCase()));
     }
@@ -32,15 +19,12 @@ exports.getAll = (search, from, to) => {
     return result;
 }
 
-exports.getOne = (cubeId) => cubes.find(x => x.id == cubeId);
+exports.getOne = (cubeId) => Cube.findById(cubeId);
 
-exports.create = (cubeData) => {
-    const newCube = {
-        id: uniqid(),
-        ...cubeData,
-    }
-    
-    cubes.push(newCube);
+exports.create = async (cubeData) => {
+    const cube = new Cube(cubeData);
 
-    return newCube;
+    await cube.save();
+
+    return cube;
 }
